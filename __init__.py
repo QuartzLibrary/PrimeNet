9e0fb79e7444fe6e9a35440a5781d1c0a2548cd2
QuartzLibrary <81446760+QuartzLibrary@users.noreply.github.com>
2025-07-10 21:56:51 +0100

    Setup package

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from . import model
+from . import model_noEpigenetics
+from . import deepprime
+from . import PRIDICT
+from . import DeepPE
+from . import Distillation
+
+__all__ = [
+    'model',
+    'model_noEpigenetics', 
+    'deepprime',
+    'PRIDICT',
+    'DeepPE',
+    'Distillation'
+]
+
+__version__ = '0.1.0'
